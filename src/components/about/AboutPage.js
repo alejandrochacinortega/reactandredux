1ef57fac3ca613251d8666a46c3ce420f7e3e454
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-05 10:47:13 +0200

    Create App, configure Routing, Entry Point, Styles, Header, and initial components

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,3 +1,14 @@
-/**
- * Created by omar on 05/08/16.
- */
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, redux, React Routet, etc</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
